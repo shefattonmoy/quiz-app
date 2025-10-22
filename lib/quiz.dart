12741be4758ff5f816c9884780f502929338d27d
shefattonmoy <shefathossain7@gmail.com>
2025-10-22 11:34:10 +0600

    added google fonts

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -12,17 +12,12 @@ class Quiz extends StatefulWidget {
 
 class _QuizState extends State<Quiz> {
 
-  Widget ? activeScreen;
+  var activeScreen = 'start-screen';
 
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -41,7 +36,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen' ? StartScreen(switchScreen) : const QuestionsScreen(),
         ),
       ),
     );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startQuiz, {super.key});
@@ -12,8 +13,8 @@ class StartScreen extends StatelessWidget {
       children: [
         Image.asset('assets/images/quiz-logo.png', width: 300, color: Color.fromARGB(150, 255, 255, 255),),
         const SizedBox(height: 80,),
-        const Text('Learn Flutter the Fun Way!', 
-        style: TextStyle(color: Color.fromARGB(255, 237, 223, 252), 
+        Text('Learn Flutter the Fun Way!',
+        style: GoogleFonts.lato(color: const Color.fromARGB(255, 237, 223, 252), 
         fontSize: 24),
         ),
         const SizedBox(height: 30),
